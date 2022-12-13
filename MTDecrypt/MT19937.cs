b1b58e9190f01978a2d0ba7408ebd291a4afff2d
Razmoth <32140579+Razmoth@users.noreply.github.com>
2022-12-13 18:16:22 +0400

    Add project files.

---
+++ MTDecrypt/MT19937.cs
@@ -0,0 +1,61 @@
+﻿namespace MTDecrypt;
+public class MT19937
+{
+    private const int N = 624;
+    private const int M = 397;
+    private const uint MATRIX_A = 0x9908B0DF;
+    private const uint UPPER_MASK = 0x80000000;
+    private const uint LOWER_MASK = 0X7FFFFFFF;
+
+    private readonly int[] mt = new int[N + 1];
+    private int mti = N + 1;
+
+    public MT19937(int seed)
+    {
+        Init(seed);
+    }
+    private void Init(int seed)
+    {
+        mt[0] = seed;
+        for (mti = 1; mti < N; mti++)
+        {
+            mt[mti] = 1812433253 * (mt[mti - 1] ^ (mt[mti - 1] >> 30)) + mti;
+        }
+    }
+
+    public int Int32()
+    {
+        int x;
+        uint[] mag01 = new uint[2] { 0x0, MATRIX_A };
+
+        if (mti >= N)
+        {
+            int kk;
+            if (mti == N + 1)
+            {
+                Init(4357);
+            }
+            for (kk = 0; kk < (N - M); kk++)
+            {
+                x = (int)(mt[kk] & UPPER_MASK | mt[kk + 1] & LOWER_MASK);
+                mt[kk] = (int)(mt[kk + M] ^ (x >> 1) ^ mag01[x & 0x1]);
+            }
+            for (; kk < N - 1; kk++)
+            {
+                x = (int)(mt[kk] & UPPER_MASK | mt[kk + 1] & LOWER_MASK);
+                mt[kk] = (int)(mt[kk + (M - N)] ^ (x >> 1) ^ mag01[x & 0x1]);
+            }
+            x = (int)(mt[N - 1] & UPPER_MASK | mt[0] & LOWER_MASK);
+            mt[N - 1] = (int)((uint)mt[M - 1] ^ (x >> 1) ^ mag01[x & 0x1]);
+
+            mti = 0;
+        }
+
+        x = mt[mti++];
+        x ^= x >> 11;
+        x ^= (int)((x << 7) & 0x9D2C5680);
+        x ^= (int)((x << 15) & 0xEFC60000);
+        x ^= x >> 18;
+        return x;
+    }
+}
---
+++ MTDecrypt/Program.cs
@@ -0,0 +1,63 @@
+﻿var inputDir = Path.Combine(Directory.GetCurrentDirectory(), "Input");
+var outputDir = Path.Combine(Directory.GetCurrentDirectory(), "Output");
+
+Directory.CreateDirectory(inputDir);
+Directory.CreateDirectory(outputDir);
+
+Console.WriteLine("Place encrypted files in Input folder, then press any key to continue");
+Console.ReadKey();
+
+var files = Directory.GetFiles(inputDir);
+Console.WriteLine($"Found {files.Length} file(s) !!");
+Console.WriteLine($"Processing...");
+foreach(var file in files)
+{
+    var fileName = Path.GetFileName(file);
+    try
+    {
+        var output = Path.Combine(outputDir, fileName);
+        var data = MTDecrypt(file);
+        File.WriteAllBytes(output, data);
+    }
+    catch(Exception e)
+    {
+        Console.WriteLine($"Unable to decrypt file {fileName}: {e.Message}");
+    }
+}
+Console.WriteLine("Done !!");
+
+static byte[] MTDecrypt(string file)
+{
+    using var fs = File.OpenRead(file);
+    using var reader = new BinaryReader(fs);
+
+    var seed = reader.ReadInt32();
+    var box = reader.ReadBytes(0x80);
+
+    var mt = new MT19937(seed);
+    var xorpad = new byte[0x200];
+    for (int i = 0; i < xorpad.Length; i += 4)
+    {
+        var seedBytes = BitConverter.GetBytes(mt.Int32());
+
+        seedBytes[0] ^= box[i % box.Length];
+        seedBytes[1] ^= box[(i + 7) % box.Length];
+        seedBytes[2] ^= box[(i + 13) % box.Length];
+        seedBytes[3] ^= box[(i + 11) % box.Length];
+
+        Buffer.BlockCopy(seedBytes, 0, xorpad, i, 4);
+    }
+
+    var xorpadIndex = mt.Int32();
+    var pos = xorpadIndex - reader.BaseStream.Position;
+
+    var data = new byte[reader.BaseStream.Length];
+    reader.BaseStream.Position = 0;
+    reader.Read(data);
+    for (int i = 0; i < data.Length; i++)
+    {
+        data[i] ^= xorpad[(pos + i) % xorpad.Length];
+    }
+
+    return data[0x84..];
+}
